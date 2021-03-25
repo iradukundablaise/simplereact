a07d442c3474a64855f8546dfd8211132b26998a
Omar Ahmani <omar.ahmani.etu@univ-lille.fr>
2021-03-25 20:45:28 +0100

    adding cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const router = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
